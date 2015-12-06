6160d82003ea859ab3d66c6f91378c887348518a
Ethan Aubuchon <ethan.a.aubuchon@gmail.com>
2015-12-06 15:01:42 -0500

    Refactor scraper to games controller

--- games/games_controller.go
+++ games/games_controller.go
@@ -1,18 +1,77 @@
-package 'game'
+package games
 
-var games map[string]games.Game
+import (
+  "strings"
+  "regexp"
+  "strconv"
+  "github.com/PuerkitoBio/goquery"
+)
+
+const SCRAPE_PATH string = "http://www.nhl.com/ice/schedulebyseason.htm"
+
+// Data Selectors
+const TABLE_ROW_SELECTOR string = "#fullPage .contentBlock table.schedTbl tbody tr"
+const TABLE_COL_SELECTOR string = "td"
+const DATE_SELECTOR string = ".skedStartDateSite"
+const TEAM_SELECTOR string = ".teamName a"
+const TEAM_SHORT_ATTR string = "rel"
+const TIME_SELECTOR string = ".skedStartTimeEST"
+const SCORE_REGEX = `FINAL:\ *[A-Z][A-Z][A-Z]\ *\(([1-9]*[0-9]+)\)\ *-\ *[A-Z][A-Z][A-Z]\ *\(([1-9]*[0-9]+)\)`
+const NUMBER_REGEX = `[1-9]*[0-9]+`
+
+
+var games map[string]Game
 
 func AddGame(game Game) {
   if games == nil {
-    games = make(map[string]games.Game)
+    games = make(map[string]Game)
   }
-  games[game.Date + game.Time + game.HomeTeam.short + game.AwayTime.short] = game
+  games[game.Date + game.Time + game.HomeTeam.Short + game.AwayTeam.Short] = game
 }
 
-func GetGame(date string, time string, homeShort string, awayShort string) (Game, boolean) {
-  if games == nil {
-    return nil, false
-  }
-  return games[string]games.Game
+// func GetGame(date string, time string, homeShort string, awayShort string) (Game, boolean) {
+//   if games == nil {
+//     return nil, false
+//   }
+//   return games[string]Game
+// }
+
+func GetGames() (map[string]Game) {
+  return games
 }
 
+func ScrapeGameData() (err error) {
+  doc, err := goquery.NewDocument(SCRAPE_PATH)
+  if err != nil {
+    return
+  }
+
+  doc.Find(TABLE_ROW_SELECTOR).Each(func(i int, s *goquery.Selection) {
+    game := Game{}
+    game.HomeTeam = Team{}
+    game.AwayTeam = Team{}
+
+    s.Find(TABLE_COL_SELECTOR).Each(func(j int, row *goquery.Selection) {
+      switch j {
+      case 0:
+        game.Date = row.Find(DATE_SELECTOR).Text()
+      case 1:
+        game.AwayTeam.Short, _ = row.Find(TEAM_SELECTOR).First().Attr(TEAM_SHORT_ATTR)
+      case 2:
+        game.HomeTeam.Short, _ = row.Find(TEAM_SELECTOR).First().Attr(TEAM_SHORT_ATTR)
+      case 3:
+        game.Time = row.Find(TIME_SELECTOR).Text()
+      case 4:
+        res := strings.Replace(row.Text(), "\n", "", -1)
+
+        if regexp.MustCompile(SCORE_REGEX).MatchString(res) == true {
+          scores := regexp.MustCompile(NUMBER_REGEX).FindAllString(res, 2)
+          game.AwayTeam.Score, _ = strconv.Atoi(scores[0])
+          game.HomeTeam.Score, _ = strconv.Atoi(scores[1])
+          AddGame(game)
+        }
+      }
+    })
+  })
+  return
+}
--- index.go
+++ index.go
@@ -2,57 +2,15 @@ package main
 
 import (
   "fmt"
-  "log"
-  "strings"
-  "regexp"
-  "strconv"
-  "github.com/PuerkitoBio/goquery"
   "github.com/ethanaubuchon/pickems_stats/games"
 )
 
-func ExampleScrape() (map[string]games.Game) {
-  g := make(map[string]games.Game)
-  doc, err := goquery.NewDocument("http://www.nhl.com/ice/schedulebyseason.htm")
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  doc.Find("#fullPage .contentBlock table.schedTbl tbody tr").Each(func(i int, s *goquery.Selection) {
-    game := games.Game{}
-    game.HomeTeam = games.Team{}
-    game.AwayTeam = games.Team{}
-
-    s.Find("td").Each(func(j int, row *goquery.Selection) {
-      switch j {
-      case 0:
-        game.Date = row.Find(".skedStartDateSite").Text()
-      case 1:
-        game.AwayTeam.Short, _ = row.Find(".teamName a").First().Attr("rel")
-      case 2:
-        game.HomeTeam.Short, _ = row.Find(".teamName a").First().Attr("rel")
-      case 3:
-        game.Time = row.Find(".skedStartTimeEST").Text()
-      case 4:
-        r := `FINAL:\ *[A-Z][A-Z][A-Z]\ *\(([1-9]*[0-9]+)\)\ *-\ *[A-Z][A-Z][A-Z]\ *\(([1-9]*[0-9]+)\)`
-
-        res := strings.Replace(row.Text(), "\n", "", -1)
-
-        if regexp.MustCompile(r).MatchString(res) == true {
-          scores := regexp.MustCompile(`[1-9]*[0-9]+`).FindAllString(res, 2)
-          game.AwayTeam.Score, _ = strconv.Atoi(scores[0])
-          game.HomeTeam.Score, _ = strconv.Atoi(scores[1])
-          g[game.Date + game.Time + game.AwayTeam.Short + game.HomeTeam.Short] = game
-        }
-      }
-    })
-  })
-  return g
-}
-
 func main() {
-  games := ExampleScrape()
+  games.ScrapeGameData()
 
-  for _, game := range games {
+  g := games.GetGames()
+
+  for _, game := range g {
     fmt.Printf("%s %s %s (%d) VS %s (%d)\n", game.Date, game.Time, game.HomeTeam.Short, game.HomeTeam.Score, game.AwayTeam.Short, game.AwayTeam.Score)
   }
 }
